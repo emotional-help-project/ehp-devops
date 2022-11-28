616e8e60e5300c700ed1f1d9cebea71adf26ff9a
Dmytro Tverdyniuk <74211759+dtverdyniuk@users.noreply.github.com>
2022-11-28 22:23:13 +0200

    Update stopjar.sh

--- stopjar.sh
+++ stopjar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+if [ $(ps aux | grep .jar | wc -l)  -gt 3 ]; then
 kill $(ps aux | grep .jar | grep sirius | head -n1 | cut -d" " -f5)    
+fi
 nohup /opt/jdk-17.0.5/bin/java -jar /tmp/project/*.jar > backend.log 2>&1 &  
