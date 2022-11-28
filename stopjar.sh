1b46c95df29aab8ad1302fac8332b5b631c6f0a0
Dmytro Tverdyniuk <74211759+dtverdyniuk@users.noreply.github.com>
2022-11-28 22:25:41 +0200

    Update stopjar.sh

--- stopjar.sh
+++ stopjar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ $(ps aux | grep .jar | wc -l)  -gt 3 ]; then
+echo "jar process detected. Killing..."
 kill $(ps aux | grep .jar | grep sirius | head -n1 | cut -d" " -f5)    
+echo "...killed"
 fi
 nohup /opt/jdk-17.0.5/bin/java -jar /tmp/project/*.jar > backend.log 2>&1 &  
