aeb2aacad786ea7d560344b2036005d370982c14
Dmytro Tverdyniuk <74211759+dtverdyniuk@users.noreply.github.com>
2022-12-11 20:47:02 +0200

    Update stopjar.sh

--- stopjar.sh
+++ stopjar.sh
@@ -4,7 +4,7 @@ lines=$(ps aux | grep .jar | wc -l)
 echo $lines
 if [[ $lines -gt 4 ]]; then
  echo "---> jar process detected. Killing..."
- kill $(ps aux | grep .jar | grep sirius | head -n1 | cut -d" " -f5)
+ kill $(ps aux | grep .jar | grep ubuntu | head -n1 | cut -d" " -f5)
  echo "...killed"
 else
  echo "---> No any jar process detected"
@@ -12,4 +12,4 @@ else
 fi
 
 echo "---> starting jar"
-nohup /opt/jdk-17.0.5/bin/java -jar /tmp/project/*.jar > ~/backend.log 2>&1 &
+nohup /opt/jdk-17.0.5/bin/java -jar /opt/backend/*.jar > ~/backend.log 2>&1 &
